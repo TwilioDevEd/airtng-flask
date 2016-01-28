533dd5707c2afd41b9d9166f8f841c6f866d8d45
nebtrx <neb.trx@gmail.com>
2016-01-28 17:54:07 -0500

    Fix issues #1

--- manage.py
+++ manage.py
@@ -8,5 +8,14 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('.', pattern="*_tests.py")
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == "__main__":
     manager.run()
--- test.py
+++
@@ -1,9 +0,0 @@
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('.', pattern="*_tests.py")
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-if __name__ == "__main__":
-    test()
