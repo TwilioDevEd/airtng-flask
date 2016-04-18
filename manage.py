c876a220799ebf74b811d6d75b0ad750344964d3
Victor Shyba <victor.shyba@gmail.com>
2016-04-18 18:44:13 -0300

    Fixes manage.py
    Closes #8

--- manage.py
+++ manage.py
@@ -12,9 +12,12 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def test():
     """Run the unit tests."""
-    import unittest
+    import sys, unittest
     tests = unittest.TestLoader().discover('.', pattern="*_tests.py")
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if not result.wasSuccessful():
+            sys.exit(1)
 
 
 if __name__ == "__main__":
